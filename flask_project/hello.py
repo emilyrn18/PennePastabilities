6387084ffb6c3dbdcf9c04857dc1e82352700f7e
emilyrn18 <emilyrn18@vt.edu>
2020-02-15 17:34:03 -0500

    lol

--- flask_project/hello.py
+++ flask_project/hello.py
@@ -7,30 +7,27 @@ from flask import render_template
 app = Flask('hello')
 @app.route('/')
 def get_recipes():
- params = ["penne"]
- app_id = "8a7451e2"
- app_key = "842fccb336c3a63726031f666fcc69ba"
+    params = ["penne"]
+    app_id = "8a7451e2"
+    app_key = "842fccb336c3a63726031f666fcc69ba"
 
+    param_string = ('&').join(params)
 
- param_string = ('&').join(params)
-
- payload = {'app_id': app_id,
+    payload = {'app_id': app_id,
                'app_key': app_key,
                'q': param_string,
                'to': 10}
 
- r = requests.get(
+    r = requests.get(
         "https://api.edamam.com/search",
         params=payload)
- edamam = r.json()
- recipes = edamam["hits"]
+    edamam = r.json()
+    recipes = edamam["hits"]
 
- for recipe in recipes:
-         name = recipe["recipe"]["label"]
-         image = recipe["recipe"]["image"]
-         url = recipe["recipe"]["url"]
-         ingredients = recipe["recipe"]["ingredientLines"]
+    for recipe in recipes:
+        name = recipe["recipe"]["label"]
+        image = recipe["recipe"]["image"]
+        url = recipe["recipe"]["url"]
+        ingredients = recipe["recipe"]["ingredientLines"]
 
- return render_template('your_view.html', your_list=recipes)
-
-
+    return render_template('your_view.html', your_list=recipes)
